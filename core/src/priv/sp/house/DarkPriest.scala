e64b933ab5f84bba8215a8dbc103e8d6b450638f
illim <illminouche@gmail.com>
2015-10-15 23:45:47 +0200

    caca boudin

--- core/src/priv/sp/house/DarkPriest.scala
+++ core/src/priv/sp/house/DarkPriest.scala
@@ -26,7 +26,7 @@ object DarkPriest {
   val ghost = DarkPriest.cards(0).asCreature
   DarkPriest initCards Houses.basicCostFunc
 
-  val oppCards = List(restlessSoul, shadowPriest, heretic)
+  val oppCards = List(restlessSoul, shadowPriest, heretic, blackAngel)
   oppCards foreach { card ⇒
     card.houseIndex = 4
     card.houseId = DarkPriest.houseId
